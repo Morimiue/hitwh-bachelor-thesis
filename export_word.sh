05edd4e2ab927f7ed5be9a3d5e185289eb14889c
cometeme <22635759+cometeme@users.noreply.github.com>
2022-05-25 01:20:41 +0800

    add citation, fignum for pandoc

--- export_word.sh
+++ export_word.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+rm output.docx || True
+
+    # --table-of-contents \
+    # --toc-depth 5 \
+
 pandoc thesis.tex \
     -F pandoc-crossref \
     --citeproc \
     -o output.docx \
     -w docx \
     --pdf-engine xelatex \
-    -M "crossrefYaml=./pandoc-crossref-es.yaml" \
+    -M "crossrefYaml=pandoc/pandoc-crossref-es.yaml" \
+    -M "reference-section-title=参考文献" \
     --bibliography=reference.bib \
-    --reference-doc ref.docx \
+    --csl pandoc/pandoc_gb7714-2005.csl \
+    --reference-doc pandoc/ref.docx \
     --top-level-division=chapter \
-    # --number-sections \
+    --number-sections \
     
